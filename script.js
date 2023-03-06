1c49f0587d1ac3250aa22515ce2da08451378fe5
Pedro Machado <pedrobmachado@hotmail.com>
2023-03-06 18:07:08 -0300

    initial js codes

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-let letters = document.querySelectorAll(".scoreboard-letter");
+let letters = document.querySelectorAll('.scoreboard-letter');
+let loading = document.getElementsByClassName('.info-bar');
+
+async function init(){
+
+
+document.addEventListener("keydown", function handleKeyPress(event){
+    const action = event.key;
+
+    console.log(action);
+
+    if(action === 'Enter'){
+        commit();
+    } else if(action === 'Backspace'){
+        backspace();
+    } else if(isLetter(action)){
+        addLetter(action.toUpperCase())
+    }else{
+        //do nothing
+    }
+});
+}
+
+function isLetter(letter) {
+    return /^[a-zA-Z]$/.test(letter);
+  }
+
+init();
