d6cd0cdb87225dca079169ae662c0df21ce9f7c1
Pedro Machado <pedrobmachado@hotmail.com>
2023-03-08 18:55:17 -0300

    Bug's Fixed

--- script.js
+++ script.js
@@ -32,7 +32,23 @@ async function commit(){
         return;
     }
     
-    
+    isLoading = true;
+    setLoading(true);
+    const res = await fetch("https://words.dev-apis.com/validate-word", {
+        method: "POST",
+        body: JSON.stringify({word: currentGuess})
+    });
+    const resObj = await res.json();
+    const validWord = resObj.validWord;
+
+    isLoading = false;
+    setLoading(false);
+
+    if (!validWord){
+        markInvalidWord();
+        return;
+    }
+
 
 const guessParts = currentGuess.split("");
 const map = makeMap(wordParts);
@@ -59,6 +75,7 @@ currentRow++;
 
 if(currentGuess === word){
     alert('you win!');
+    document.querySelector('.brand').classList.add("winner");
     done = true;
     return;
 }else if (currentRow === ROUNDS) {
@@ -75,7 +92,15 @@ function backspace(){
     letters[ANSWER_LENGTH * currentRow + currentGuess.length].innerText = "";
 }
 
+function markInvalidWord(){
+    for(let i = 0; i < ANSWER_LENGTH; i++){
+        letters[currentRow * ANSWER_LENGTH + i].classList.remove("invalid");
 
+        setTimeout(function (){
+            letters[currentRow * ANSWER_LENGTH + i].classList.add("invalid");
+        }, 10);
+    }
+}
 
 
 document.addEventListener("keydown", function handleKeyPress(event){
